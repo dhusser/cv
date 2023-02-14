ad1758c03fc79dbc5bd43ca236b3421ec47c20db
Denis Gusser <104307530+dhusser@users.noreply.github.com>
2023-02-14 18:06:52 +0400

    fix directory

--- js/script.js
+++ js/script.js
@@ -43,21 +43,21 @@ function createListWithlink(ulName, items) {
 }
 
 // add menu items
-getJson('../assets/json/menu.json').then(response => {
+getJson('./assets/json/menu.json').then(response => {
   const hardSkillsList = document.querySelector('.nav')
   hardSkillsList.append(createListWithlink(response.name, response.items))
 
 })
 
 // add Hard skills
-getJson('../assets/json/hard-skills.json').then(response => {
+getJson('./assets/json/hard-skills.json').then(response => {
   const hardSkillsList = document.querySelector('.hard-skills')
   hardSkillsList.append(createList(response.name, response.items))
 
 })
 
 // add Language skills
-getJson('../assets/json/language-skills.json').then(response => {
+getJson('./assets/json/language-skills.json').then(response => {
   const hardSkillsList = document.querySelector('.language-skills')
   hardSkillsList.append(createList(response.name, response.items))
 })
